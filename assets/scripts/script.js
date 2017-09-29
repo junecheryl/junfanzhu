45c3974c236e08b23f0e2a578b471a0f74c8d821
Junfan Zhu <junecheryl0113@gmail.com>
2017-09-28 20:08:40 -0700

    /Users/June/Desktop/Web/dev

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,10 @@
 console.log("A boilerplate project including full setup for Jekyll, GulpJS, SASS, PostCSS, BrowserSync and deploy to gh-pages using Gulp by Esau Silva.");
+$(document).ready(menu);
+
+function menu() {
+  $('#menu-icon-shape').on('click', function() {
+    $('#menu-icon-shape').toggleClass('active');
+    $('#top, #middle, #bottom').toggleClass('active');
+    $('#overlay-nav').toggleClass('active');
+  });
+}
